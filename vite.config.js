03fab0ca6c3fd57b9d892f7ae651182d8b19b055
YungzyPwC <phoenix.abran1@gmail.com>
2025-03-24 15:25:25 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/XeroSaver/'  // This matches your repository name
+  base: './',  // Change this line from '/XeroSaver/' to './'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
